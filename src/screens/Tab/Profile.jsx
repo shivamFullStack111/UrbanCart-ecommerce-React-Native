b348f2e647de99a1de0c7863c10a602affbe6b0c
shivam <shvam12340987@gmail.com>
2026-02-14 00:44:29 +0530

    change project from expo cli to expo and now it is working properly

--- src/screens/Tab/Profile.jsx
+++ src/screens/Tab/Profile.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/self-closing-comp */
-import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
-import React from 'react';
-import {ScrollView} from 'react-native-gesture-handler';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {
   faBell,
   faCommentsDollar,
@@ -11,9 +11,9 @@ import {
   faMessage,
   faStar,
   faUser,
-} from '@fortawesome/free-solid-svg-icons';
-import {gray, manImage} from '../../utils';
-import {useNavigation} from '@react-navigation/native';
+} from "@fortawesome/free-solid-svg-icons";
+import { gray, manImage } from "../../utils";
+import { useNavigation } from "@react-navigation/native";
 
 const Profile = () => {
   const navigation = useNavigation();
@@ -25,8 +25,9 @@ const Profile = () => {
           <Text style={styles.headerTitle}>Urban Cart</Text>
         </View>
         <TouchableOpacity
-          onPress={() => navigation.navigate('notifications')}
-          style={styles.notificationContainer}>
+          onPress={() => navigation.navigate("notifications")}
+          style={styles.notificationContainer}
+        >
           <FontAwesomeIcon size={26} icon={faBell} />
           <Text style={styles.notificationBadge}>1</Text>
         </TouchableOpacity>
@@ -35,7 +36,7 @@ const Profile = () => {
       {/* image with 4 box */}
       <View style={styles.imageContainer}>
         <View style={styles.profileRow}>
-          <Image style={styles.profileImage} source={{uri: manImage}} />
+          <Image style={styles.profileImage} source={{ uri: manImage }} />
           <Text style={styles.greetingText}>
             Hello, <Text style={styles.userName}>Shivam</Text>
           </Text>
@@ -45,25 +46,29 @@ const Profile = () => {
         <View style={styles.boxContainer}>
           <View style={styles.boxRow}>
             <TouchableOpacity
-              onPress={() => navigation.navigate('orders')}
-              style={styles.box}>
+              onPress={() => navigation.navigate("orders")}
+              style={styles.box}
+            >
               <Text style={styles.boxText}>Your Order</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => navigation.navigate('wishlist')}
-              style={styles.box}>
+              onPress={() => navigation.navigate("wishlist")}
+              style={styles.box}
+            >
               <Text style={styles.boxText}>Wishlist</Text>
             </TouchableOpacity>
           </View>
           <View style={styles.boxRow}>
             <TouchableOpacity
-              onPress={() => navigation.navigate('coupons')}
-              style={styles.box}>
+              onPress={() => navigation.navigate("coupons")}
+              style={styles.box}
+            >
               <Text style={styles.boxText}>Coupons</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => navigation.navigate('orders')}
-              style={styles.box}>
+              onPress={() => navigation.navigate("orders")}
+              style={styles.box}
+            >
               <Text style={styles.boxText}>Track Order</Text>
             </TouchableOpacity>
           </View>
@@ -74,17 +79,18 @@ const Profile = () => {
         <Text style={styles.sectionTitle}>Account Settings</Text>
 
         <TouchableOpacity
-          onPress={() => navigation.navigate('editProfile')}
-          style={styles.settingRow}>
+          onPress={() => navigation.navigate("editProfile")}
+          style={styles.settingRow}
+        >
           <FontAwesomeIcon size={20} icon={faUser} color="red" />
           <Text style={styles.settingText}>Edit Profile</Text>
-          <Text style={styles.arrow}>></Text>
+          <Text style={styles.arrow}></Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.settingRow}>
           <FontAwesomeIcon color="red" size={20} icon={faCreditCard} />
           <Text style={styles.settingText}>Saved Cards & Wallet</Text>
-          <Text style={styles.arrow}>></Text>
+          <Text style={styles.arrow}></Text>
         </TouchableOpacity>
 
         {/* <TouchableOpacity
@@ -92,15 +98,16 @@ const Profile = () => {
           style={styles.settingRow}>
           <FontAwesomeIcon color="red" size={20} icon={faMapLocationDot} />
           <Text style={styles.settingText}>Saved Addresses</Text>
-          <Text style={styles.arrow}>></Text>
+          <Text style={styles.arrow}></Text>
         </TouchableOpacity> */}
 
         <TouchableOpacity
-          onPress={() => navigation.navigate('accountDetails')}
-          style={styles.settingRow}>
+          onPress={() => navigation.navigate("accountDetails")}
+          style={styles.settingRow}
+        >
           <FontAwesomeIcon color="red" size={20} icon={faCommentsDollar} />
           <Text style={styles.settingText}>Account Details</Text>
-          <Text style={styles.arrow}>></Text>
+          <Text style={styles.arrow}></Text>
         </TouchableOpacity>
       </View>
 
@@ -110,13 +117,13 @@ const Profile = () => {
         <View style={styles.activityRow}>
           <FontAwesomeIcon size={20} color="red" icon={faMessage} />
           <Text style={styles.activityText}>Reviews</Text>
-          <Text style={styles.arrow}>></Text>
+          <Text style={styles.arrow}></Text>
         </View>
 
         <View style={styles.activityRow}>
           <FontAwesomeIcon size={20} icon={faStar} color="red" />
           <Text style={styles.activityText}>Ratings</Text>
-          <Text style={styles.arrow}>></Text>
+          <Text style={styles.arrow}></Text>
         </View>
       </View>
     </ScrollView>
@@ -126,25 +133,25 @@ const Profile = () => {
 const styles = StyleSheet.create({
   scrollContainer: {
     flexGrow: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     paddingBottom: 60,
   },
   header: {
-    width: '100%',
-        marginTop:30,
+    width: "100%",
+    marginTop: 30,
 
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     padding: 12,
     paddingVertical: 15,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     elevation: 5,
   },
   headerTitle: {
     fontSize: 24,
-    color: 'black',
-    fontWeight: '800',
+    color: "black",
+    fontWeight: "800",
   },
   notificationContainer: {
     gap: 5,
@@ -152,61 +159,61 @@ const styles = StyleSheet.create({
   notificationBadge: {
     height: 17,
     width: 17,
-    backgroundColor: 'red',
-    position: 'absolute',
+    backgroundColor: "red",
+    position: "absolute",
     right: -5,
     borderRadius: 20,
     top: -5,
-    textAlign: 'center',
-    fontWeight: '600',
-    color: 'white',
+    textAlign: "center",
+    fontWeight: "600",
+    color: "white",
   },
   imageContainer: {
     padding: 10,
     marginTop: 10,
   },
   profileRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 15,
-    alignItems: 'center',
+    alignItems: "center",
   },
   profileImage: {
     width: 80,
     height: 80,
     borderRadius: 200,
     borderWidth: 1,
-    backgroundColor: 'black',
+    backgroundColor: "black",
   },
   greetingText: {
-    color: 'black',
+    color: "black",
     fontSize: 20,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   userName: {
-    color: 'red',
+    color: "red",
   },
   boxContainer: {
     marginTop: 20,
     gap: 15,
   },
   boxRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
     // justifyContent: 'space-between',
     gap: 12,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   box: {
     backgroundColor: gray.extraLight,
-    color: 'black',
+    color: "black",
     width: 160,
     height: 52,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   boxText: {
-    color: 'black',
+    color: "black",
     fontSize: 16,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   settingsContainer: {
     padding: 10,
@@ -214,36 +221,36 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 20,
     color: gray.gray,
-    fontWeight: '800',
+    fontWeight: "800",
   },
   settingRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 8,
     marginTop: 15,
-    alignItems: 'center',
+    alignItems: "center",
   },
   settingText: {
     color: gray.gray,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
   },
   arrow: {
-    marginLeft: 'auto',
-    color: 'black',
+    marginLeft: "auto",
+    color: "black",
   },
   activityContainer: {
     padding: 10,
   },
   activityRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 8,
     marginTop: 15,
-    alignItems: 'center',
+    alignItems: "center",
   },
   activityText: {
     color: gray.gray,
     fontSize: 20,
-    fontWeight: '400',
+    fontWeight: "400",
   },
 });
 
